b35e5c145c575997aa001cc4634320096c0f51f8
Huy <tranbaminhhuy@gmail.com>
2024-11-05 08:13:29 -0500

    Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -37,10 +37,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.google.firebase:firebase-auth:21.1.0")
-    implementation("com.google.firebase:firebase-firestore:24.5.0")
-    implementation(platform("com.google.firebase:firebase-bom:33.5.1"))
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    id("com.google.gms.google-services") version "4.4.2" apply false
+    alias(libs.plugins.google.gms.google.services) apply false
 }
 
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("com.google.gms:google-services:4.3.15")
-    }
-}
